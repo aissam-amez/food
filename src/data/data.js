0a983f1420dd5ae3a81295338ef74181bb3d2157
Aissam amezaourou <aissam@mit-academy.net>
2021-01-03 18:49:23 +0100

    - Add some data for testing

---
+++ src/data/data.js
@@ -0,0 +1,70 @@
+const business = [
+    {
+        imageSrc: 'https://media-cdn.tripadvisor.com/media/photo-s/16/2b/ac/60/poke-bowl.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flavortown',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'Italian',
+        rating: 4.5,
+        reviewCount: 111
+    },
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'snak hassan',
+        address: 'daoudiat',
+        city: 'marrakech',
+        state: 'marrakech',
+        zipCode: '40000',
+        category: 'chainise',
+        rating: 4,
+        reviewCount: 165
+    },
+    {
+        imageSrc: 'https://localadventurer.com/wp-content/uploads/2015/11/what-to-eat-in-morocco.jpg',
+        name: 'otacos',
+        address: 'guiliz',
+        city: 'Flavortown',
+        state: 'marrakech',
+        zipCode: '10101',
+        category: 'morrocan',
+        rating: 4.2,
+        reviewCount: 510
+    },
+    {
+        imageSrc: 'https://food.hmizate.ma/image/cache/catalog/Hmizatefood-banner-Chilis-480x250.jpg',
+        name: 'snak chouaib',
+        address: 'lw7da lkhamsa',
+        city: 'marrakech',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'Italian, farensh',
+        rating: 4.6,
+        reviewCount: 950
+    },
+    {
+        imageSrc: 'https://res.cloudinary.com/hv9ssmzrz/image/fetch/c_fill,f_auto,h_600,q_auto,w_800/https://s3-eu-west-1.amazonaws.com/images-ca-1-0-1-eu/tag_photos/original/9121/fast_food_flickr_6319155216_0463fda84a_b.jpg',
+        name: 'frensh food',
+        address: 'guiliz',
+        city: 'marrakech',
+        state: 'NYc',
+        zipCode: '10101',
+        category: 'Italian, frensh, chainise',
+        rating: 4.7,
+        reviewCount: 10
+    },
+    {
+        imageSrc: 'https://static.atlantico.fr/sites/default/files/styles/media_image_449x268/public/images/2018/10/000_1501mx.jpg?itok=DkjkZXjL',
+        name: ' without name',
+        address: '1010  Way',
+        city: 'Fortown',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'maghribi',
+        rating: 3.9,
+        reviewCount: 90
+    }
+]
+
+export default business
