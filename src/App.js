1a48a47f41fd30a8038892b975dfb641c3d83db7
Aissam amezaourou <aissam@mit-academy.net>
2021-01-03 19:04:53 +0100

    - Change app.js it represent home page - Add style for app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './components/app/App.css';
+import BusinessList from './components/BusinessList/BusinessList'
+import SearchBar from './components/searchBar/SearchBar'
+import search from './util/Yelp'
+//import business from './data/data'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [resturent, setResturent] = useState([])
+
+	const searchYelp = (term, location, sortBy) =>{
+		//let resturents = resturent
+		// if(location !== ''){
+		// 	resturents = Object.values(resturents).filter( resto => resto.city === location  )
+		// }
+		// if(term !== ''){
+		// 	resturents = Object.values(resturents).filter( resto => resto.name.indexOf(term) !== -1 )
+		// }
+		// if(sortBy === 'rating'){
+		// 	resturents = Object.values(resturents).sort((c, d)=> c.rating < d.rating)
+		// }else if(sortBy === 'reviewCount' ){
+		// 	resturents = Object.values(resturents).sort((c, d)=> c.reviewCount < d.reviewCount)
+		// }
+		//console.log(sortBy)
+		search(term, location, sortBy).then(businesses => {
+			if(businesses){
+				businesses = businesses.map(business => {
+					return {
+						id : business.id,
+						imageSrc: business.image_url,
+				        name: business.name,
+				        address: business.location.address1,
+				        city: business.city,
+				        state: business.state,
+				        zipCode: business.zip_code,
+				        category: business.categories[0] ? business.categories[0].title : '',
+				        rating: business.rating ? business.rating : 0 ,
+				        reviewCount: business.review_count ? business.review_count : 0 ,
+					}
+				})
+				setResturent(businesses)
+			}
+		 })
+	}
+
+  	return (
+		<>
+			<SearchBar searching={searchYelp} />
+			<BusinessList businesses={resturent} />
+		</>
+	);
 }
 
 export default App;
